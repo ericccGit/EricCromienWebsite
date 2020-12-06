f1cea6a753b98a5dab8771b836e4ecbaa505549f
Eric Cromien <ecromien@uwaterloo.ca>
2020-12-06 13:24:04 -0500

    add logo images and poc code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  const peices = [];
+  
+  
+  for(let i = 0; i < 6; i++){
+   peices.push({cName: `AnimatedLogoPeice-${i}`, src: `/Logo_${30 + 60*i}.svg`})
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Logo-area">
+        {peices && peices.map(p => {return (
+          <div className={p.cName}>
+          <div className="App-logo-filler"/>
+          <img src={p.src} className="logo-peice" alt="logo" />
+        </div>
+        )})}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+  /*
+<div className="AnimatedLogoPeice-0">
+          <div className="App-logo-filler"/>
+          <img src="/Logo_30.svg" className="logo-peice" alt="logo" />
+        </div>
+
+
+        <div className="AnimatedLogoPeice">
+          <div className="App-logo-filler"/>
+          <img src="/Logo_90.svg" className="logo-peice" alt="logo" />
+        </div>
+        <div className="AnimatedLogoPeice">
+          <div className="App-logo-filler"/>
+          <img src="/Logo_150.svg" className="logo-peice" alt="logo" />
+        </div>
+        <div className="AnimatedLogoPeice">
+          <div className="App-logo-filler"/>
+          <img src="/Logo_210.svg" className="logo-peice" alt="logo" />
+        </div>
+        <div className="AnimatedLogoPeice">
+          <div className="App-logo-filler"/>
+          <img src="/Logo_270.svg" className="logo-peice" alt="logo" />
+        </div>
+        <div className="AnimatedLogoPeice">
+          <div className="App-logo-filler"/>
+          <img src="/Logo_330.svg" className="logo-peice" alt="logo" />
+        </div>
+        */
